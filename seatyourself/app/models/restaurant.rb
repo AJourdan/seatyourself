b86039a5c6e689f3ca666fbc675175440dfe051c
Alon Jourdan <alon.jourdan@gmail.com>
2015-11-18 17:17:09 -0500

    Add sign up page

--- seatyourself/app/models/restaurant.rb
+++ seatyourself/app/models/restaurant.rb
@@ -1,2 +1,7 @@
 class Restaurant < ActiveRecord::Base
+	has_many :reservations
+	has_many :users, through :reservations
+	belongs_to :owner, class_name: "user", foreign_key "user_id"
+	belongs_to :category
+	
 end
--- seatyourself/app/models/user.rb
+++ seatyourself/app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ActiveRecord::Base
+	has_many :reservations
+	has_many :restaurants, through: :reservations
+
+	has_one :owned_restaurant, class_name: "Restaurant"
 end
