803333bee22ccb6e0f4f7878f7bc6ba70a05842b
Alon Jourdan <alon.jourdan@gmail.com>
2015-11-20 15:37:38 -0500

    Working Reservation

--- seatyourself/app/controllers/reservations_controller.rb
+++ seatyourself/app/controllers/reservations_controller.rb
@@ -1,15 +1,25 @@
 class ReservationsController < ApplicationController
+
+
+def new
+	@restaurant = Restaurant.find(params[:restaurant_id])
+	@reservation = Reservation.new
 end
 
-
  def create
-    @reservation = @user.reservation.build(reservation_params)
+ 	@restaurant = Restaurant.find(params[:restaurant_id])
+    @reservation = @restaurant.reservations.build(params[:reservation_params])
     @reservation.user = current_user
 
-    if @rerservation.save
-      redirect_to reservations_path, notice: 'Reservation successfully made'
+    if @reservation.save
+      redirect_to restaurant_path(@restaurant)
     else
-      render ''
+      render 'new'
     end
   end
 
+  private
+  def reservation_params
+  	params.require(:reservation).permit(:number_of_guests, :date_time)
+  end
+end
--- seatyourself/config/routes.rb
+++ seatyourself/config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
     resources :users
-    resources :restaurants
+    resources :restaurants do 
+      resources :reservations      
+    end
     resources :sessions, only: [:new, :create, :destroy]
-    resources :reservations
-    
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
